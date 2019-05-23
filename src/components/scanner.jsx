7905390d9008334aa2ca1dbbdafd1fa991aac16d
Pasindu Chandrasekara <mail2psc@gmail.com>
2019-05-23 16:32:58 +0530

    Initial Commit

--- src/components/scanner.jsx
+++ src/components/scanner.jsx
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
 import QrReader from "react-qr-reader";
+import { Route, Redirect } from "react-router";
 
 class Scanner extends Component {
   state = {
     result: "No result"
   };
 
+  componentDidMount() {
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+        {
+          video: true
+        },
+        function(localMediaStream) {},
+        function(err) {
+          alert(
+            "The following error occurred when trying to access the camera: " +
+              err
+          );
+        }
+      );
+    } else {
+      alert("Sorry, browser does not support camera access");
+    }
+  }
+
   handleScan = data => {
     if (data) {
+      var obj = JSON.parse(data);
       this.setState({
-        result: data
+        result: obj.bearerToken
       });
+
+      window.location.href = "https://www.youtube.com/";
     }
   };
 
   handleError = err => {
-    console.error(err);
+    alert(err);
   };
 
   render() {
